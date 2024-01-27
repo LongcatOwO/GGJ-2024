a730d717f11f383538bc3c76ef471afb7c04954a
LongcatOwO <greatphu@gmail.com>
2024-01-27 15:02:57 +1300

    Implemented pickup

--- Assets/Scripts/WeaponPicker.cs
+++ Assets/Scripts/WeaponPicker.cs
@@ -3,32 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Character))]
 [RequireComponent(typeof(PlayerInputHandler))]
-public class WeaponFinder : MonoBehaviour
+public class WeaponPicker : MonoBehaviour
 {
+    Character _character;
     PlayerInputHandler _inputHandler;
-    SlammingWeapon _weaponInRange;
+    DroppedWeapon _weaponInRange;
 
     void Awake()
     {
+        _character = GetComponent<Character>();
         _inputHandler = GetComponent<PlayerInputHandler>();
+        _inputHandler.OnPickupInput += OnPickupInput;
     }
 
-    SlammingWeapon GetWeaponInRange()
+    void OnPickupInput()
     {
-        return _weaponInRange;
+        if (_weaponInRange == null) return;
+        var heldWeapon = Instantiate(_weaponInRange.Info.HeldForm).GetComponent<SlammingWeapon>();
+        _character.PickupWeapon(heldWeapon);
+        Destroy(_weaponInRange.gameObject);
+        _weaponInRange = null;
     }
 
     void OnTriggerEnter(Collider collider)
     {
-        var weapon = collider.GetComponent<SlammingWeapon>();
+        var weapon = collider.GetComponentInParent<DroppedWeapon>();
         if (weapon == null) return;
         _weaponInRange = weapon;
     }
 
     void OnTriggerExit(Collider collider)
     {
-        var weapon = collider.GetComponent<SlammingWeapon>();
+        var weapon = collider.GetComponentInParent<DroppedWeapon>();
         if (_weaponInRange == weapon)
             _weaponInRange = null;
     }
