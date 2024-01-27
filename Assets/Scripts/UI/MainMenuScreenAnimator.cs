0931e409da5bca20e7fc61a28d10f1807bb8c663
Sylky <80878564+Limeyuzu@users.noreply.github.com>
2024-01-27 18:47:48 +1300

    merge title and menu screen

--- Assets/Scripts/UI/MainMenuScreenAnimator.cs
+++ Assets/Scripts/UI/MainMenuScreenAnimator.cs
@@ -2,35 +2,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class TitleScreenAnimator : MonoBehaviour
+public class MainMenuScreenAnimator : MonoBehaviour
 {
     [SerializeField] Texture2D[] Textures;
     [SerializeField] float AnimationSpeed = 20f;
     [SerializeField] int NextScreenTriggerFrame = 25;
 
-    private RawImage _rawImage;
+    [SerializeField] RawImage RawImage;
+    [SerializeField] Slider StartButtonSlider;
+    [SerializeField] TitleStartButton StartButton;
 
-    private bool _holdingInput;
     private float _heldTime;
     private int _currentFrameIndex;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        PlayerInputHandler.Instance.OnAttackInputDown += () => _holdingInput = true;
-        PlayerInputHandler.Instance.OnAttackInputUp += () => _holdingInput = false;
-
-        _rawImage = GetComponent<RawImage>();
-    }
-
     private void Update()
     {
-        if (_holdingInput)
+        if (StartButton.IsButtonHeld)
         {
             _heldTime += Time.deltaTime;
-
-            _currentFrameIndex = Mathf.Min(Mathf.FloorToInt(_heldTime * AnimationSpeed), Textures.Length - 1);
-            _rawImage.texture = Textures[_currentFrameIndex];
+            UpdateLoadValue(_heldTime);
         } 
         else
         {
@@ -51,9 +41,15 @@ public class TitleScreenAnimator : MonoBehaviour
             {
                 Debug.Log("Stopped at frame " + _currentFrameIndex);
             }
-            _currentFrameIndex = 0;
             _heldTime = 0;
-            _rawImage.texture = Textures[0];
+            UpdateLoadValue(_heldTime);
         }
+    }
+
+    private void UpdateLoadValue(float heldTime)
+    {
+        _currentFrameIndex = Mathf.Min(Mathf.FloorToInt(heldTime * AnimationSpeed), Textures.Length - 1);
+        RawImage.texture = Textures[_currentFrameIndex];
+        StartButtonSlider.value = heldTime * AnimationSpeed / (NextScreenTriggerFrame - 1);
     }
 }
