c06b3d579323d37abb12ecbd80a4024ebe66bca5
lukeypookie111 <lukevanzuilem@gmail.com>
2024-01-27 20:22:06 +1300

    Adding fish weapon
    Fish weapon files added

--- Assets/Scripts/Entities/SeaOfHappiness.cs
+++ Assets/Scripts/Entities/SeaOfHappiness.cs
@@ -6,25 +6,23 @@ using UnityEngine.SceneManagement;
 public class SeaOfHappiness : MonoBehaviour
 {
     //This class acts to destroy all objects the enters its collider, sending them to "a happy place".
-
-    [SerializeField] private Score scoreComponent;
-
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.TryGetComponent(out PlayerController playerController))
+        GameObject scoreObject = GameObject.Find("ScoreObj");
+        if (other == null)
         {
-            //player 1 scores
-            if (playerController.IsSecondaryPlayer)
-            {
-                scoreComponent.Player1Score++;
-            }
-            //player 2 scores
-            else
-            {
-                scoreComponent.Player2Score++;
-            }
+            return;
         }
+        //player 1 scores
+        if (other.gameObject.GetComponent<PlayerController>().IsSecondaryPlayer)
+        {
+            scoreObject.GetComponent<Score>().Player1Score++;
 
+        }
+        else //player 2 scores
+        {
+            scoreObject.GetComponent<Score>().Player2Score++;
+        }
         Destroy(other.gameObject);
 
         //insert UI stuff (Game Over screen here)
---
+++ Assets/Scripts/Weaponry/FishSlapWeapon.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class FishSlapWeapon : MonoBehaviour
+{
+    //This class handles the activation of the "slamming weapon" as well as its hit effects.
+
+    public WeaponInfo Info { get { return info; } }
+
+    [Header("Weapon Reference")]
+    [SerializeField] private WeaponInfo info;
+
+    [Header("Component References")]
+    [SerializeField] private AttackAnimationEvents attackEvents;
+    [SerializeField] private Collider hostCollider;
+    [SerializeField] private Collider weaponCollider;
+
+    [Header("Hit Properties")]
+    [SerializeField] private float buryPotential;
+
+    private GameObject hitTargetGameObject;
+    private float slamForce;
+
+    private void Start()
+    {
+        weaponCollider = GetComponent<Collider>();
+
+        Physics.IgnoreCollision(hostCollider, weaponCollider);
+
+        weaponCollider.enabled = false;
+    }
+
+    //Registers the events that will invoke the methods of this weapon.
+    private void OnEnable()
+    {
+        SubscribeAttackExecutorEvents();
+    }
+    
+    //Unregisters the events that will invoke the methods of this weapon.
+    private void OnDisable()
+    {
+        UnsubscribeAttackExecutorEvents();
+    }
+
+    //If a viable target enters the weapon's collider, trigger the hit effect.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Character>() != null || other.GetComponent<SlammableTarget>() != null)
+        {
+            if(hitTargetGameObject == other.gameObject)
+            {
+                return;
+            }
+
+            hitTargetGameObject = other.gameObject;
+
+            hitTargetGameObject.transform.position += Vector3.right * slamForce * buryPotential;
+        }
+    }
+
+    private void SubscribeAttackExecutorEvents()
+    {
+        if (attackEvents == null) return;
+
+        attackEvents.OnAttackExecuted += InitializeAttack;
+
+        attackEvents.OnAttackEnded += EndAttack;
+    }
+
+    private void UnsubscribeAttackExecutorEvents()
+    {
+        if (attackEvents == null) return;
+
+        attackEvents.OnAttackExecuted -= InitializeAttack;
+
+        attackEvents.OnAttackEnded -= EndAttack;
+    }
+
+    //Initializes the weapon's component references.
+    public void InitializeWeapon(GameObject hostGameObject)
+    {
+        hostCollider = hostGameObject.GetComponent<Collider>();
+
+        attackEvents = hostGameObject.GetComponentInChildren<AttackAnimationEvents>();
+
+        SubscribeAttackExecutorEvents();
+    }
+
+    //Readies the weapon for attack. Enables the weapon's collider to allow "OnTriggerEnter()" calls.
+    public void InitializeAttack(float slamForce)
+    {
+        this.slamForce = slamForce;
+
+        weaponCollider.enabled = true;
+    }
+
+    //Disables the weapon's attack functionality by disabling the weapon's collider.
+    public void EndAttack()
+    {
+        weaponCollider.enabled = false;
+
+        hitTargetGameObject = null;
+    }
+}
