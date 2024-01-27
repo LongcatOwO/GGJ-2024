0d20c136506ea317e46f8c289ce88ba0de85435e
Sylky <80878564+Limeyuzu@users.noreply.github.com>
2024-01-27 19:26:16 +1300

    Get keyboard hold on start button working

--- Assets/Scripts/UI/Buttons/TitleStartButton.cs
+++ Assets/Scripts/UI/Buttons/TitleStartButton.cs
@@ -2,26 +2,30 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class TitleStartButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class TitleStartButton : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
-    public bool IsButtonHeld => _buttonHeld;
+    public bool IsButtonHeld => _buttonHeld && _buttonSelected;
 
     private bool _buttonHeld;
+    private bool _buttonSelected;
 
     // Start is called before the first frame update
     void Start()
     {
         var button = GetComponent<Button>();
         button.Select();
+
+        PlayerInputHandler.Instance.OnAttackInputDown += () => _buttonHeld = true;
+        PlayerInputHandler.Instance.OnAttackInputUp += () => _buttonHeld = false;
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    public void OnSelect(BaseEventData eventData)
     {
-        _buttonHeld = true;
+        _buttonSelected = true;
     }
 
-    public void OnPointerUp(PointerEventData eventData)
+    public void OnDeselect(BaseEventData eventData)
     {
-        _buttonHeld = false;
+        _buttonSelected = false;
     }
 }
