ae55bb6a0c5f333faec561526bda967b2c846622
LongcatOwO <greatphu@gmail.com>
2024-01-27 13:52:55 +1300

    Created WeaponInfo

--- Assets/Scripts/WeaponPicker.cs
+++ Assets/Scripts/WeaponPicker.cs
@@ -3,31 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
-[RequireComponent(typeof(Character))]
 [RequireComponent(typeof(PlayerInputHandler))]
-public class WeaponPicker : MonoBehaviour
+public class WeaponFinder : MonoBehaviour
 {
-    Character _character;
     PlayerInputHandler _inputHandler;
     SlammingWeapon _weaponInRange;
 
     void Awake()
     {
-        _character = GetComponent<Character>();
         _inputHandler = GetComponent<PlayerInputHandler>();
-        _inputHandler.OnPickupInput += OnPickupInput;
     }
 
     SlammingWeapon GetWeaponInRange()
     {
         return _weaponInRange;
-    }
-
-    void OnPickupInput()
-    {
-        if (_weaponInRange == null) return;
-        _character.PickupWeapon(_weaponInRange);
-        _weaponInRange = null;
     }
 
     void OnTriggerEnter(Collider collider)
